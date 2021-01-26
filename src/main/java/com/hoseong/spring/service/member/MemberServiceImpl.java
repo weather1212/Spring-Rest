8bf471f55cc4389de33f7c90046c9d6f5fa25420
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-01-26 20:47:14 +0900

    Update MemberServiceImpl.java

--- src/main/java/com/hoseong/spring/service/member/MemberServiceImpl.java
+++ src/main/java/com/hoseong/spring/service/member/MemberServiceImpl.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Service;
 
 import com.hoseong.spring.dao.board.BoardDAO;
+import com.hoseong.spring.dao.member.MemberDAO;
 import com.hoseong.spring.vo.board.BoardVO;
 import com.hoseong.spring.vo.member.MemberVO;
 
@@ -15,24 +16,36 @@ import com.hoseong.spring.vo.member.MemberVO;
 public class MemberServiceImpl implements MemberService {
 
 	@Inject
-	private BoardDAO boardDAO;
+	private MemberDAO memberDao;
 
+	// 회원 로그인 체크
 	@Override
 	public boolean loginCheck(MemberVO vo, HttpSession session) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean result = memberDao.loginCheck(vo);
+
+		if (result) { // true일 경우 세션에 등록
+			MemberVO vo2 = viewMember(vo);
+
+			// 세션 변수 등록
+			session.setAttribute("userId", vo2.getUserId());
+			session.setAttribute("userName", vo2.getUserName());
+		}
+		return result;
 	}
 
+	// 회원 로그인 정보
 	@Override
 	public MemberVO viewMember(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		return memberDao.viewMember(vo);
 	}
 
+	// 회원 로그아웃
 	@Override
 	public void logout(HttpSession session) {
-		// TODO Auto-generated method stub
-
+		// 세션 개별 삭제
+//		session.removeAttribute("userId");
+		// 세션 정보를 초기화
+		session.invalidate();
 	}
 
 }
