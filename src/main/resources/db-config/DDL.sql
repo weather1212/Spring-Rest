30d15c7e7c322ec3c9a652f7f300cd3a62399afb
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-01-30 23:21:55 +0900

    Update DDL.sql
    reply 테이블 및 reply_seq 생성 쿼리

--- src/main/resources/db-config/DDL.sql
+++ src/main/resources/db-config/DDL.sql
@@ -23,3 +23,19 @@ user_email varchar2(200) not null,
 user_regdate date default sysdate,
 user_updatedate date
 );
+
+
+-- reply 테이블 생성
+create table reply (
+rno number not null constraint reply_rno_pk primary key,
+bno number not null,
+replytext varchar2(4000),
+replyer varchar2(50) not null,
+regdate date default sysdate,
+updatedate date
+);
+
+-- reply rno 시퀀스 생성
+create sequence reply_seq
+start with 1
+increment by 1;
