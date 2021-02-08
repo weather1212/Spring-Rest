0e2fc5f57edb6eccc5e8bceec243f493771a45d9
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-02-08 23:47:49 +0900

    AJAX 파일업로드 업로드한 파일표시, 썸네일 출력, 다운로드, 삭제
    *업로드한 파일의 확장자를 확인해 이미지파일이면 썸네일 출력, 이미지파일이 아니면 다운로드 구현

--- src/main/java/com/hoseong/spring/controller/upload/UploadController.java
+++ src/main/java/com/hoseong/spring/controller/upload/UploadController.java
@@ -1,14 +1,19 @@
 package com.hoseong.spring.controller.upload;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hoseong.spring.util.MediaUtils;
 import com.hoseong.spring.util.UploadFileUtils;
 
 @Controller
@@ -32,14 +38,18 @@ public class UploadController {
 	String uploadPath;
 
 	// 업로드 흐름 : 업로드 버튼 클릭 => 임시 디렉토리에 업로드 => 지정된 디렉토리에 저장 => 파일 정보가 file에 저장
-	
-	/****************************** # 일반적인 방식의 업로드 처리  *********************************/
-	
+
+	/******************************
+	 * # 일반적인 방식의 업로드 처리
+	 *********************************/
+
+	// 일반적인 업로드 매핑(GET)
 	@RequestMapping(value = "/upload/uploadForm", method = RequestMethod.GET)
 	public void uploadForm() {
 		// upload/uploadForm.jsp (업로드 페이지)로 포워딩
 	}
 
+	// 일반적인 업로드 매핑(POST)
 	@RequestMapping(value = "/upload/uploadForm", method = RequestMethod.POST)
 	public ModelAndView updateForm(MultipartFile file, ModelAndView mav) throws Exception {
 
@@ -49,8 +59,9 @@ public class UploadController {
 
 		String saveName = file.getOriginalFilename(); // 파일의 원본 이름
 
-		saveName = uploadFile(saveName, file.getBytes());	// uuid_원본이름 저장
+		saveName = uploadFile(saveName, file.getBytes()); // uuid_원본이름 저장
 
+		// 파일 경로, 파일명을 받아 파일 객체 생성
 		File target = new File(uploadPath, saveName);
 
 		// 임시디렉토리에 저장된 업로드된 파일을 지정된 디렉토리로 복사
@@ -73,17 +84,22 @@ public class UploadController {
 
 		return saveName;
 	}
-	
-	
-	/****************************** # 일반적인 방식의 업로드 처리  *********************************/
-	
-	/****************************** # ajax 방식의 업로드 처리  *********************************/
-	
+
+	/******************************
+	 * # 일반적인 방식의 업로드 처리END
+	 *********************************/
+
+	/******************************
+	 * # ajax 방식의 업로드 처리
+	 *********************************/
+
+	// Ajax방식 파일 업로드 페이지 매핑(GET)
 	@RequestMapping(value = "/upload/uploadAjax", method = RequestMethod.GET)
 	public void uploadAjax() {
 		// uploadAjax.jsp로 포워딩
 	}
-	
+
+	// Ajax방식 파일 업로드 페이지 매핑(POST)
 	// produces = "text/plain; charset=utf-8" : 파일 한글 처리
 	@ResponseBody
 	@RequestMapping(value = "/upload/uploadAjax", method = RequestMethod.POST, produces = "text/plain; charset=utf-8")
@@ -91,8 +107,74 @@ public class UploadController {
 		logger.info("파일이름 : " + file.getOriginalFilename());
 		logger.info("파일크기 : " + file.getSize());
 		logger.info("컨텐트 타입: " + file.getContentType());
-		
-		return new ResponseEntity<String>(UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()), HttpStatus.OK);
+
+		return new ResponseEntity<String>(
+				UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()), HttpStatus.OK);
+	}
+
+	// 이미지 표시 매핑
+	@ResponseBody // view를 리턴하지 않고 data를 리턴
+	@RequestMapping("/upload/displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName) throws IOException {
+		// 서버의 파일을 다운로드하기 위한 스트림
+		InputStream in = null; // java.io
+		ResponseEntity<byte[]> entity = null;
+		try {
+			// 확장자를 추출하여 formatName에 저장
+			String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+			// 추출한 확장자를 MediaUtils 클래스에서 이미지파일 여부를 검사하고 리턴받아 mType에 저장
+			MediaType mType = MediaUtils.getMediaType(formatName);
+			// 헤더 구성 객체 (외부에서 데이터를 주고받을 때에는 header와 body를 구성해야하기 때문에)
+			HttpHeaders headers = new HttpHeaders();
+			// InputStream 생성
+			in = new FileInputStream(uploadPath + fileName);
+			// 이미지파일 이면
+			if (mType != null) {
+				headers.setContentType(mType);
+			} else { // 이미지파일이 아니면
+				fileName = fileName.substring(fileName.indexOf("_") + 1);
+				// 다운로드용 컨텐트 타입
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+				// 바이트배열을 스트링으로 : new String(fileName.getBytes("utf-8"),"iso-8859-1")
+				// *iso-8859-1 서유럽언어, 큰 따옴표 내부에 "\" 내용 \" "
+				// 파일의 한글 깨짐 방지
+				headers.add("Content-Disposition",
+						"attachment; filename=\"" + new String(fileName.getBytes("utf-8"), "iso-8859-1") + "\"");
+				// headers.add("Content-Disposition", "attachment; filename='"+fileName+"'");
+			}
+			// 바이트배열, 헤더, HTTP상태코드
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), headers, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			// HTTP상태 코드()
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		} finally {
+			in.close(); // 스트림 닫기
+		}
+		return entity;
+	}
+
+	// 파일 삭제 매핑
+	@ResponseBody // view를 리턴하지 않고 data를 리턴
+	@RequestMapping(value = "/upload/deleteFile", method = RequestMethod.POST)
+	public ResponseEntity<String> deleteFile(String fileName) {
+		// 파일의 확장자 추출
+		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+		// 이미지 파일 여부 검사
+		MediaType mType = MediaUtils.getMediaType(formatName);
+		// 이미지의 경우(썸네일 + 원본파일 삭제), 이미지가 아니면 원본파일만 삭제
+		if (mType != null) { // 이미지 파일이면
+			// 썸네일 이미지 파일 추출
+			String front = fileName.substring(0, 12);
+			String end = fileName.substring(14);
+			// 썸네일 이미지 삭제
+			new File(uploadPath + (front + end).replace('/', File.separatorChar)).delete();
+		}
+		// 원본파일 삭제
+		new File(uploadPath + fileName.replace('/', File.separatorChar)).delete();
+
+		// 데이터와 http 상태 코드 전송
+		return new ResponseEntity<String>("deleted", HttpStatus.OK);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/upload/uploadAjax.jsp
+++ src/main/webapp/WEB-INF/views/upload/uploadAjax.jsp
@@ -56,10 +56,84 @@ small {
 				contentType : false,
 				success : function(data) {
 					alert(data);
+					var str = "";
+					// 이미지 파일이면 썸네일 이미지 출력
+					if(checkImageType(data)) {
+						str = "<div><a href='${path}/upload/displayFile?fileName=" + getImageLink(data) + "'>";
+						str += "<img src='${path}/upload/displayFile?fileName=" + data + "'></a>";
+					} else {	// 일반 파일이면 다운로드 링크
+						str="<div><a href='${path}/upload/displayFile?fileName=" + data + "'>" + getOriginalName(data) + "</a>";
+						
+					}
+					// 삭제버튼
+					str += "<span data-src=" + data + ">[삭제]</span></div>";
+					$(".uploadedList").append(str);
 				}
 			});
 
 		});
+		
+		// 파일 삭제 요청 처리
+		$(".uploadedList").one("click", "span", function(event) {
+			alert("이미지 삭제")
+			var that = $(this);	// this 는 클릭한 span태그
+			$.ajax({
+				type: "post",
+				url: "${path}/upload/deleteFile",
+				// data: "fileName=" + $(this).attr("data-src") = {fileName:$(this).attr("data-src")}
+				// 태그.attr(속성)
+				data: {fileName:$(this).attr("data-src")},	// json방식
+				dataType: "text",
+				success: function(result) {
+					if(result == "deleted") {
+						// 클릭한 태그가 속한 div를 제거
+						that.parent("div").remove();
+					}
+				}
+				
+			});
+		});
+		
+		//===================파일 관련 스크립트=============================
+			
+		// 원본 파일 이름을 목록에 출력
+		function getOriginalName(fileName) {
+			// 이미지 파일이면
+			if(checkImageType(fileName)) {
+				return;	//함수 종료
+			}
+			
+			// uuid를 제외한 원래 파일이름을 리턴
+			var idx = fileName.indexOf("_")+1;
+			return fileName.substr(idx);
+		}
+		
+		// 이미지파일 링크 - 클릭하면 원본 이미지 출력
+		function getImageLink(fileName) {
+			// 이미지파일 아니면
+			if(!checkImageType(fileName)) {
+				return;	//함수 종료
+			}
+			// 이미지 파일이면 (썸네일이 아닌 원본이미지를 가져오기 위해)
+			// 썸네일 이미지 파일명 => 파일경로 + 파일명 => /2021/02/08/s_109db3b6-b4c5-48f7-bc0b-e9478f84bb28_캐나다 밴쿠버.jpg
+			var front = fileName.substr(0, 12);	// 년월일 경로 추출
+			var end = fileName.substr(14);	// 년월일 경로와 s_를 제거한 원본 파일명을 추출
+			
+			console.log(front);	// /2021/02/08/
+			console.log(end);	// 109db3b6-b4c5-48f7-bc0b-e9478f84bb28_캐나다 밴쿠버.jpg
+			
+			// 원본 파일명 => /2021/02/08/109db3b6-b4c5-48f7-bc0b-e9478f84bb28_캐나다 밴쿠버.jpg
+			return front+end;	// 디렉토리를 포함한 원본파일명을 리턴 
+		}
+		
+		// 이미지 파일 형식 체크
+		function checkImageType(fileName) {
+			// i : ignore case(대소문자 무관)
+			var pattern = /jpg|gif|png|jpeg/i;	// 정규표현식
+		
+			return fileName.match(pattern);	// 규칙이 맞으면 true리턴
+		}
+		
 	});
 </script>
 </head>
